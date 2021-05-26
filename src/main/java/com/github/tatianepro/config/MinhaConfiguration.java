ccaffdf71b79603f4bdcc2ae3fa4f8ec7126e805
Tatiane <tatiane.takeuti@outlook.com>
2021-05-26 08:53:44 -0300

    Creating custom annotation

--- src/main/java/com/github/tatianepro/config/MinhaConfiguration.java
+++ src/main/java/com/github/tatianepro/config/MinhaConfiguration.java
@@ -1,12 +1,12 @@
 package com.github.tatianepro.config;
 
+import com.github.tatianepro.annotation.Development;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Configuration
-@Profile("development")
+@Development
 public class MinhaConfiguration {
 
     @Bean   // annotation that scans the object to be used in the application context
