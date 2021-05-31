87a70542bff298a710e4c1611834c5de84637ce7
Tatiane <tatiane.takeuti@outlook.com>
2021-05-31 15:39:29 -0300

    Implements JWT Filter to intercept requests and provide security context access

---
+++ src/main/java/com/github/tatianepro/security/jwt/JwtAuthFilter.java
@@ -0,0 +1,54 @@
+package com.github.tatianepro.security.jwt;
+
+import com.github.tatianepro.rest.service.impl.UsuarioServiceImpl;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// Implementing JWT Filter to intercept requests
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private JwtService jwtService;
+    private UsuarioServiceImpl usuarioService;
+
+    public JwtAuthFilter(JwtService jwtService, UsuarioServiceImpl usuarioService) {
+        this.jwtService = jwtService;
+        this.usuarioService = usuarioService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String authorization = request.getHeader("Authorization");
+        if (!authorization.isEmpty() && authorization.startsWith("Bearer")) {
+            String token = authorization.split(" ")[1];
+            boolean tokenIsValido = jwtService.tokenIsValido(token);
+            if (tokenIsValido) {
+                String loginUsuario = jwtService.obterLoginUsuario(token);
+                // load user details with his roles
+                UserDetails userDetails = usuarioService.loadUserByUsername(loginUsuario);
+                // stores an authenticated user details to provide access to the security context
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new
+                        UsernamePasswordAuthenticationToken(userDetails,
+                                                    null,
+                                                               userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken
+                        .setDetails(new WebAuthenticationDetailsSource()
+                        .buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+}
