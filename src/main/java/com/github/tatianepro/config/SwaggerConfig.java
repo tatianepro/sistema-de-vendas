9a0598ef85a4b24d39432f1d4bb97c05c5f05c60
Tatiane <tatiane.takeuti@outlook.com>
2021-06-01 11:57:22 -0300

    Implementing Swagger Header Authorization

--- src/main/java/com/github/tatianepro/config/SwaggerConfig.java
+++ src/main/java/com/github/tatianepro/config/SwaggerConfig.java
@@ -4,13 +4,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableSwagger2
@@ -29,7 +33,6 @@ public class SwaggerConfig {
     @Value("${swagger.version}")
     private String version;
 
-
     @Bean
     public Docket docket() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -37,7 +40,15 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.github.tatianepro.rest.controller"))
                 .paths(PathSelectors.any())
-                .build()
+                .build().globalOperationParameters(Arrays.asList(
+                        new ParameterBuilder()
+                                .name("Authorization")
+                                .description("Header para token JWT")
+                                .modelRef(new ModelRef("string"))
+                                .parameterType("header")
+                                .required(false)
+                                .build()
+                ))
                 .apiInfo(apiInfo());
     }
 
